b445e9a3d377ba68157532a0d5e415e45e068892
fabianallendorf <fabian.allendorf@node.energy>
2023-05-15 22:26:08 +0200

    adding options for copyable models

--- django_model_deepcopy/register.py
+++ django_model_deepcopy/register.py
@@ -1,11 +1,25 @@
+import dataclasses
+
+from django.db import models
+
+
+@dataclasses.dataclass(frozen=True)
+class CopyableModelOptions:
+    fields: list[str] = dataclasses.field(default_factory=list)
+    exclude: list[str] = dataclasses.field(default_factory=list)
+
+
 class CopyableModelRegister:
     def __init__(self):
         self._registry = {}
 
-    def register(self, model):
-        self._registry[model] = {}
+    def register(
+        self, model: models.Model, options: CopyableModelOptions | None = None
+    ):
+        self._registry[model] = options or CopyableModelOptions()
 
-    def unregister(self, model):
+    def unregister(self, model: models.Model):
         del self._registry[model]
+
 
 register = CopyableModelRegister()
