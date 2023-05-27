0e58d536fed92b3662d8840a18e22465f11c6570
fabianallendorf <fabian.allendorf@node.energy>
2023-05-27 23:21:27 +0200

    add pyright

--- django_model_deepcopy/deepcopy.py
+++ django_model_deepcopy/deepcopy.py
@@ -6,3 +6,4 @@ from django.db import models
 def deepcopy(instance: models.Model) -> models.Model:
     copied_instance = copy.deepcopy(instance)
     copied_instance.pk = None
+    return copied_instance
